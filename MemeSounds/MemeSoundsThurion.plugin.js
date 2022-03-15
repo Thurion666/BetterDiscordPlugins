97dcb5e9e08dda44c9c46c0df9fa55be338dd8e0
Thurion666 <38357989+Thurion666@users.noreply.github.com>
2022-03-15 22:52:02 +0100

    Update MemeSoundsThurion.plugin.js

--- MemeSounds/MemeSoundsThurion.plugin.js
+++ MemeSounds/MemeSoundsThurion.plugin.js
@@ -1,6 +1,6 @@
 /**
  * @name MemeSoundsThurion
- * @version 0.6.5
+ * @version 0.6.6
  * @description Plays Memetastic sounds depending on what is being sent in chat. This was heavily inspired by the idea of Metalloriff's bruh plugin so go check him out!
  * @author Thurion#8885
  * @authorId 219612697002508288
@@ -12,7 +12,7 @@
 module.exports = (() => {
 	
 	/* Configuration */
-	const config = {info: {name: "Meme Sounds Thurion", authors: [{name: "Thurion#8885", discord_id: "219612697002508288", github_username: "Thurion666", twitter_username: "Thurión"}], version: "0.6.5", description: "Plays Memetastic sounds depending on what is being sent in chat. This was heavily inspired by the idea of Metalloriff's bruh plugin so go check him out!", github: "https://github.com/Thurion666/BetterDiscordPlugins/blob/main/MemeSounds/MemeSoundsThurion.plugin.js", github_raw: "https://raw.githubusercontent.com/Thurion666/BetterDiscordPlugins/main/MemeSounds/MemeSoundsThurion.plugin.js"}, defaultConfig: [{id: "setting", name: "Sound Settings", type: "category", collapsible: true, shown: true, settings: [{id: "LimitChan", name: "Limit to the current channel only.", note: "When enabled, sound effects will only play within the currently selected channel.", type: "switch", value: true}, {id: "delay", name: "Sound effect delay.", note: "The delay in miliseconds between each sound effect.", type: "slider", value: 200, min: 10, max: 1000, renderValue: v => Math.round(v) + "ms"}, {id: "volume", name: "Sound effect volume.", note: "How loud the sound effects will be.", type: "slider", value: 1, min: 0.01, max: 1, renderValue: v => Math.round(v*100) + "%"}]}], changelog: [{title: "New Stuff", items: ["Fixed typo mistake"]}]};
+	const config = {info: {name: "Meme Sounds Thurion", authors: [{name: "Thurion#8885", discord_id: "219612697002508288", github_username: "Thurion666", twitter_username: "Thurión"}], version: "0.6.6", description: "Plays Memetastic sounds depending on what is being sent in chat. This was heavily inspired by the idea of Metalloriff's bruh plugin so go check him out!", github: "https://github.com/Thurion666/BetterDiscordPlugins/blob/main/MemeSounds/MemeSoundsThurion.plugin.js", github_raw: "https://raw.githubusercontent.com/Thurion666/BetterDiscordPlugins/main/MemeSounds/MemeSoundsThurion.plugin.js"}, defaultConfig: [{id: "setting", name: "Sound Settings", type: "category", collapsible: true, shown: true, settings: [{id: "LimitChan", name: "Limit to the current channel only.", note: "When enabled, sound effects will only play within the currently selected channel.", type: "switch", value: true}, {id: "delay", name: "Sound effect delay.", note: "The delay in miliseconds between each sound effect.", type: "slider", value: 200, min: 10, max: 1000, renderValue: v => Math.round(v) + "ms"}, {id: "volume", name: "Sound effect volume.", note: "How loud the sound effects will be.", type: "slider", value: 1, min: 0.01, max: 1, renderValue: v => Math.round(v*100) + "%"}]}], changelog: [{title: "New Stuff", items: ["Fixed typo on depression sound"]}]};
 
 	/* Library Stuff */
 	return !global.ZeresPluginLibrary ? class {
@@ -67,7 +67,7 @@ module.exports = (() => {
 				{re: /eat/gmi, file: "minecraft eat.mp3", duration: 1800},
 				{re: /nokia/gmi, file: "nokia.mp3", duration: 1800},
 				{re: /waiting/gmi, file: "waiting elevator.mp3", duration: 1800},
-				{re: /depresssion/gmi, file: "crippling depression.mp3", duration: 1800},
+				{re: /depression/gmi, file: "crippling depression.mp3", duration: 1800},
 				{re: /virus/gmi, file: "computer virus.mp3", duration: 1800},
 				{re: /oh pog/gmi, file: "oh pog.mp3", duration: 1800},
 				{re: /aña/gmi, file: "pekka aña.mp3", duration: 1800}
